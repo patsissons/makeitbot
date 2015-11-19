23bf1fa486377b26dd89a6f28002766076db9f69
Pat Sissons <psissons@acrotech.io>
2015-11-18 21:30:45 -0800

    adding hubot-youtube plugin

--- bin/slack.cmd
+++ bin/slack.cmd
@@ -3,5 +3,6 @@
 REM Set your slack token here to connect the bot to your live slack team
 REM You can find this on the Heroku settings tab (Config Variables)
 SET HUBOT_SLACK_TOKEN=
+SET HUBOT_YOUTUBE_API_KEY=
 
 node_modules\.bin\hubot.cmd --adapter slack
