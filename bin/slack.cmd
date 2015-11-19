afd696d0f64101f7da2bf0fb7e0328eafd1e5801
Pat Sissons <psissons@acrotech.io>
2015-11-18 22:00:45 -0800

    adding hubot-darksky package (forecast.io)

--- bin/slack.cmd
+++ bin/slack.cmd
@@ -4,5 +4,7 @@ REM Set your slack token here to connect the bot to your live slack team
 REM You can find this on the Heroku settings tab (Config Variables)
 SET HUBOT_SLACK_TOKEN=
 SET HUBOT_YOUTUBE_API_KEY=
+SET HUBOT_DARK_SKY_API_KEY=
+SET HUBOT_DARK_SKY_DEFAULT_LOCATION=
 
 node_modules\.bin\hubot.cmd --adapter slack
